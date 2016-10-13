b084fae3c8795973b4775039d7bb368e16eab0f2
BalazsMarton <balazsmarton90.joomla@gmail.com>
2016-10-13 03:05:36 +0200

    navbar js

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -2,4 +2,8 @@ jQuery(document).ready(function($){
     $('.parallax').parallax();
 	  var clientHeight = $( window ).height();
 	  $('.element').css('height', clientHeight);
+	    // Initialize collapse button
+  $(".button-collapse").sideNav();
+  // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+  //$('.collapsible').collapsible();
 });
