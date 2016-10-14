e651f18d17439055d2b40a72a28770420c91b62e
BalazsMarton <balazsmarton90.joomla@gmail.com>
2016-10-14 02:30:11 +0200

    scrollfire js added

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -11,4 +11,22 @@ document.addEventListener("turbolinks:load", function() {
 	$(".button-collapse").sideNav();
 	// Initialize collapsible (uncomment the line below if you use the dropdown variation)
 	//$('.collapsible').collapsible();
+
+	//Scrollfire
+	  var options = [
+    {selector: '#staggered-test', offset: 50, callback: function(el) {
+      Materialize.toast("This is our ScrollFire Demo!", 1500 );
+    } },
+    {selector: '#staggered-test', offset: 205, callback: function(el) {
+      Materialize.toast("Please continue scrolling!", 1500 );
+    } },
+    {selector: '#staggered-test', offset: 400, callback: function(el) {
+      Materialize.showStaggeredList($(el));
+    } },
+    {selector: '#image-test', offset: 500, callback: function(el) {
+      Materialize.fadeInImage($(el));
+    } }
+  ];
+  Materialize.scrollFire(options);
+
 })
